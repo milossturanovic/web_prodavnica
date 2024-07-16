1d17052d1651b09c657b3792fb13185dc9cd1862
unknown <sturanovic.ms@gmail.com>
2024-07-16 23:42:58 +0200

    dodao mogucnost registracije i prijave korisnika

--- js/main.js
+++ js/main.js
@@ -224,3 +224,74 @@
 
 
 })(jQuery);
+
+
+
+
+function user_register(){
+	jQuery('.field_error').html('');
+	var name=jQuery("#name").val();
+	var email=jQuery("#email").val();
+	var mobile=jQuery("#mobile").val();
+	var password=jQuery("#password").val();
+	var is_error='';
+	if(name==""){
+		jQuery('#name_error').html('Molimo unesite ime');
+		is_error='yes';
+	}if(email==""){
+		jQuery('#email_error').html('Unesite email');
+		is_error='yes';
+	}if(mobile==""){
+		jQuery('#mobile_error').html('Unesite broj telefona');
+		is_error='yes';
+	}if(password==""){
+		jQuery('#password_error').html('Unesite sifru');
+		is_error='yes';
+	}
+	if(is_error==''){
+		jQuery.ajax({
+			url:'register_submit.php',
+			type:'post',
+			data:'name='+name+'&email='+email+'&mobile='+mobile+'&password='+password,
+			success:function(result){
+				if(result=='email_present'){
+					jQuery('#email_error').html('Email je vec u bazi');
+				}
+				if(result=='insert'){
+					jQuery('.register_msg p').html('Hvala na registraciji');
+				}
+			}	
+		});
+	}
+	
+}
+
+
+function user_login(){
+	jQuery('.field_error').html('');
+	var email=jQuery("#login_email").val();
+	var password=jQuery("#login_password").val();
+	var is_error='';
+	if(email==""){
+		jQuery('#login_email_error').html('Uensite email');
+		is_error='yes';
+	}if(password==""){
+		jQuery('#login_password_error').html('Unesite password');
+		is_error='yes';
+	}
+	if(is_error==''){
+		jQuery.ajax({
+			url:'login_submit.php',
+			type:'post',
+			data:'email='+email+'&password='+password,
+			success:function(result){
+				if(result=='wrong'){
+					jQuery('.login_msg p').html('Uneesite login detalje');
+				}
+				if(result=='valid'){
+					window.location.href='index.php';
+				}
+			}	
+		});
+	}	
+}
